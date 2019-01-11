be44ae0df773184e312c6dc773f0f139df239277
Liam Owen <l1amow3n5@yahoo.com>
2019-01-11 12:38:55 -0700

    added the shiftLeftRight method to Pictue.java

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -150,6 +150,32 @@ public class Picture extends SimplePicture
 	  }
   }
   
+  public void shiftLeftRight(int amount)
+  {
+	  Pixel [][] pixels = this.getPixels2D();
+	  Picture temp = new Picture(this);
+	  Pixel [][] copied = temp.getPixels2D();
+	  
+	  int shiftedValue = amount;
+	  int width = pixels[0].length;
+	  
+	  for(int row = 0 ; row < pixels.length; row++)
+	  {
+		  for (int col = 0; col < pixels[0].length; col++)
+		  {
+			  shiftedValue = (col + amount) % width;
+			  copied[row][col].setColor(pixels[row][shiftedValue].getColor());
+		  }
+	  }
+	  for (int row = 0; row < pixels.length; row++)
+	  {
+		  for (int col = 0; col < pixels[0].length; col++)
+		  {
+			  pixels[row][col].setColor(copied[row][col].getColor());
+		  }
+	  }
+  }
+  
   public void randomColor()
   {
 	  int randomInt = (int)(Math.random() * 3);
